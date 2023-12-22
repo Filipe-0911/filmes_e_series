fa22396331354ba98567747195e21d2532ffd4ac
Filipe <fbianchi.andrade@gmail.com>
2023-12-22 17:11:37 -0300

    corrigido o bug da playlist, a lista nao estava sendo limpa antes de adicionar os filmes com os generos filtrados

--- classes/AnaliseCategoria.py
+++ classes/AnaliseCategoria.py
@@ -8,18 +8,17 @@ class AnaliseCategoria(Crud):
     def receber_dados(self):
         filmes = Crud().consultar_dados('Filme')
         series = Crud().consultar_dados('Serie')
+        self._programas.clear()
         
         lista_com_todos_os_produtos = []
-
         lista_com_todos_os_produtos.extend(filmes)
-        
         lista_com_todos_os_produtos.extend(series)
         
         dados = self._agrupa_dados(lista_com_todos_os_produtos)
         
         for dado in dados:
             self._programas.append(dado)
-    
+        
     def _agrupa_dados(self, lista_produtos):
         dados_agrupados = []
 
@@ -29,7 +28,7 @@ class AnaliseCategoria(Crud):
 
                 if isinstance(categorias, str):   
                     categorias_filtradas = [cat for cat in categorias.split(', ') if cat.lower() == self.nome.lower()]
-    
+                    
                     if categorias_filtradas:
                         dados_agrupados.append({'id':f"{row[0]}",'nome':f"{row[1]}", 'categorias':f"{categorias_filtradas[0]}"})
                 else:
